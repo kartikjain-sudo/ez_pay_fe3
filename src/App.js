24d614a5d22da01beac5727813b7b6a30a45e292
kartikjn <kartikjain42@protonmail.com>
2023-12-09 21:06:36 +0530

    integrating wallet

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-
 import React from "react";
 import "./App.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Navbar from "./components/Navbar";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/BorrowRequest";
 import Events from "./pages/LendingRequest";
- 
+
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/BorrowRequest" element={<About />} />
-                <Route
-                    path="/LendingRequest"
-                    element={<Events />}
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: 12,
+        }}
+      >
+        <ConnectButton />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/BorrowRequest" element={<About />} />
+        <Route path="/LendingRequest" element={<Events />} />
+      </Routes>
+    </Router>
+  );
 }
- 
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,56 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  goerli,
+  polygonMumbai,
+  polygonZkEvmTestnet,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    polygonZkEvmTestnet,
+    goerli,
+    polygonMumbai,
+    ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [alchemyProvider({ apiKey: "ak_CLXNUvyLcX7YFGVqKSzy2pVT4DDel" }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'EZ Pay',
+  projectId: '5a9849f3d0cf4cbca60f51a71bded5f1',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+});
  
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 );
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
         <App />
-    </React.StrictMode>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
 );
