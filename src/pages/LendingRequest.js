6c0462f72031f502f4cb41aa6592c85d7d31c49e
kartikjn <kartikjain42@protonmail.com>
2023-12-09 19:50:42 +0530

    navbar

---
+++ src/pages/LendingRequest.js
@@ -0,0 +1,18 @@
+import React from "react";
+ 
+const LendingRequest = () => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+            }}
+        >
+            <h1>Annual Report</h1>
+        </div>
+    );
+};
+ 
+export default LendingRequest;
